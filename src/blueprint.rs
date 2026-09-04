23b087013cae66f10528f8ff3e545f737c5909df
sagi <sagi21805@gmail.com>
2026-09-04 18:43:06 +0300

    add marker type expension

--- src/blueprint.rs
+++ src/blueprint.rs
@@ -1,6 +1,6 @@
 use proc_macro2::TokenStream;
 use quote::{format_ident, quote};
-use syn::{ItemTrait, TraitItem};
+use syn::{ItemTrait, TraitItem, TraitItemType, parse_quote};
 
 pub struct Blueprint<'ast> {
     inner: &'ast ItemTrait,
@@ -13,14 +13,35 @@ impl<'ast> Blueprint<'ast> {
 
     pub fn create_group_marker(&self) -> TokenStream {
         let marker_name = format_ident!("{}GroupMarker", &self.inner.ident);
-        let type_definitions: Vec<&TraitItem> = self
+        let type_definitions: Vec<&TraitItemType> = self
             .inner
             .items
             .iter()
-            .filter(|i| matches!(i, TraitItem::Type(_)))
+            .filter_map(|i| {
+                if let TraitItem::Type(t) = i {
+                    Some(t)
+                } else {
+                    None
+                }
+            })
             .collect();
 
+        let type_idents = type_definitions.iter().map(|t| &t.ident);
+
+        let mut original = self.inner.clone();
+
+        let marker_type = parse_quote! {
+            type Marker: #marker_name<
+                #( #type_idents = Self::#type_idents ),*
+            >;
+        };
+
+        original.items.push(TraitItem::Type(marker_type));
+
         quote! {
+
+            #original
+
             trait #marker_name {
                 #(#type_definitions)*
             }
