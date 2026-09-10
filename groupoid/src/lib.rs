b7302d70dd2089535c2096168794ac8d0bd20466
sagi <sagi21805@gmail.com>
2026-09-10 20:06:44 +0300

    Add tests module

--- groupoid/src/lib.rs
+++ groupoid/src/lib.rs
@@ -1,14 +1,13 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub trait State {}
+
+pub trait WithState {
+    type State: State;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub trait Group {}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub trait GroupMarker {
+    type Marker: Group;
 }
+
+pub use groupoid_macros::*;
---
+++ tests/src/lib.rs
@@ -0,0 +1,23 @@
+use std::marker::PhantomData;
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+struct Test<T> {
+    _marker: PhantomData<T>,
+}
+
+impl<T> groupoid::WithState for Test<T> {
+    type State = T;
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
